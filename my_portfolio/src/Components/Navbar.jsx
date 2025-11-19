2e43bddc627816aa0651631c9eabc29cb3aabf20
Olamide Akinleye <olamideakinleye800@gmail.com>
2025-11-19 17:00:34 +0100

    adjust

--- my_portfolio/src/Components/Navbar.jsx
+++ my_portfolio/src/Components/Navbar.jsx
@@ -3,8 +3,8 @@ function Navbar() {
     const [isOpen, setIsOpen] = useState(false)
     return(
         <header className="sticky top-0 z-50 bg-white">
-            <div className="max-w-4xl mx-auto px-6 ">
-                <div className="flex items-center justify-between py-4">
+            <div className="max-w-4xl mx-auto px-4 ">
+                <div className="flex items-center justify-between py-6">
                     <p className="font-bold text-lg text-green-700">Olamide Akinleye</p>
                     <nav className="hidden md:flex gap-6 items-center">
                     <a href="#home" className="font-semibold hover:bg-green-400 rounded-lg px-2">Home</a>
@@ -12,7 +12,7 @@ function Navbar() {
                     <a href="#skills" className="font-semibold hover:bg-green-400 rounded-lg px-2">Skills</a>
                     <a href="#projects" className="font-semibold hover:bg-green-400 rounded-lg px-2">Projects</a>
                     <a href="#contact" className="font-semibold hover:bg-green-400 rounded-lg px-2">Contact</a>
-                    <a href="#" className="ml-3 inline-flex items-center gap-2 px-3 py-2 rounded-full border border-green-300 text-md text-green-700 hover:bg-green-200">Github</a>
+                    <a href="#" className="ml-3 inline-flex items-center gap-2 px-3 py-2 rounded-lg border border-green-300 text-md text-green-700 hover:bg-green-200">Github</a>
                     </nav>
                 
                     <button onClick={() => setIsOpen(!isOpen)} className="text-gray-700 text-2xl font-bold md:hidden">
