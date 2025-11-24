4c72af80364315073418989e9511d6b3784f331d
Olamide Akinleye <olamideakinleye800@gmail.com>
2025-11-24 15:59:39 +0100

    made an adjustment

--- my_portfolio/src/Components/Navbar.jsx
+++ my_portfolio/src/Components/Navbar.jsx
@@ -25,11 +25,11 @@ function Navbar() {
                 {isOpen && (
                     <div className="py-4 border-t md:hidden">
                         <div className="flex flex-col gap-4">
-                            <a href="#home" className="font-semibold hover:bg-green-400 rounded-lg px-2">Home</a>
-                            <a href="#about" className="font-semibold hover:bg-green-400 rounded-lg px-2">About</a>
-                            <a href="#skills" className="font-semibold hover:bg-green-400 rounded-lg px-2">Skills</a>
-                            <a href="#projects" className="font-semibold hover:bg-green-400 rounded-lg px-2">Projects</a>
-                            <a href="#contact" className="font-semibold hover:bg-green-400 rounded-lg px-2">Contact</a>
+                            <a href="#home" onClick={() => setActive('home')} className={activeLink("home")}>Home</a>
+                            <a href="#about" onClick={() => setActive('about')} className={activeLink("about")}>About</a>
+                            <a href="#skills" onClick={() => setActive('skills')} className={activeLink('skills')}>Skills</a>
+                            <a href="#projects" onClick={() => setActive('projects')} className={activeLink('projects')}>Projects</a>
+                            <a href="#contact" onClick={() => setActive('contact')} className={activeLink('contact')}>Contact</a>
                         </div>
                     </div>
                 )}
